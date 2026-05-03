3cfb586aad303b6ee8c9edb948892fb2a40a1c30
OlenaDovh <lothenshow@gmail.com>
2026-05-03 14:44:56 +0300

    chore: prepare bot for Railway deployment

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-from dotenv import load_dotenv
 import json
 import os
+from dotenv import load_dotenv
 from google.oauth2 import service_account
 
 load_dotenv()
@@ -8,11 +8,11 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
 GOOGLE_CLOUD_LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+credentials = None
 
-if credentials_json:
-    credentials_dict = json.loads(credentials_json)
-    credentials = service_account.Credentials.from_service_account_info(credentials_dict)
-else:
-    credentials = None
+if GOOGLE_CREDENTIALS_JSON:
+    credentials_info = json.loads(GOOGLE_CREDENTIALS_JSON)
+    credentials = service_account.Credentials.from_service_account_info(credentials_info)
